33e953f0bffc77a1436127b61163927ceda1dbe6
woohoo1607 <woohoo1607@gmail.com>
2021-01-28 11:40:35 +0200

    feat: Style select input and add phone inputs

--- src/components/SelectInput/index.js
+++ src/components/SelectInput/index.js
@@ -2,11 +2,55 @@ import React from "react";
 import Select from "react-select";
 import { useFormikContext } from "formik";
 
-const SelectInput = ({ options, name, value, ...props }) => {
+const SelectInput = ({ options, name, value, isError, ...props }) => {
   const { setFieldValue } = useFormikContext();
+  const customStyles = {
+    control: (provided, state) => {
+      let borderColor =
+        (isError && "#EB5757") || (state.isFocused && "#A1C4FF") || "#C1CFE0";
+      return {
+        ...provided,
+        border: `1px solid ${borderColor}`,
+        borderRadius: 0,
+        backgroundColor: "#FFFFFF",
+        height: "40px",
+        boxShadow: "none",
+        "&:hover": { border: `1px solid ${borderColor}` },
+      };
+    },
+    valueContainer: () => ({
+      height: "40px",
+      paddingLeft: "10px",
+    }),
+    input: () => ({
+      marginTop: "9px",
+    }),
+    menuList: (base) => ({
+      ...base,
+      height: "172px",
+      color: "#657C9A",
+      "::-webkit-scrollbar": {
+        width: "2px",
+      },
+      "::-webkit-scrollbar-track": {
+        background: "none",
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: "#C1CFE0",
+        borderRadius: "29px",
+      },
+    }),
+    option: (provided, { isSelected, isFocused }) => ({
+      ...provided,
+      color: "#657C9A",
+      backgroundColor: isSelected ? "#E7F0FF" : isFocused ? "#E7F0FF" : null,
+    }),
+  };
+
   return (
     <Select
       options={options}
+      placeholder=""
       {...props}
       name={name}
       value={options ? options.find((option) => option.value === value) : ""}
@@ -15,6 +59,7 @@ const SelectInput = ({ options, name, value, ...props }) => {
         DropdownIndicator: () => null,
         IndicatorSeparator: () => null,
       }}
+      styles={customStyles}
     />
   );
 };
--- src/components/UserForms/ContactsForm.js
+++ src/components/UserForms/ContactsForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as Yup from "yup";
+import { useFormikContext } from "formik";
 
 import LayoutForm from "./LayoutForm";
 import Input from "../UI/Input";
@@ -8,11 +9,23 @@ import FormikField from "../FormikField";
 import InputContainer from "../UI/InputContainer";
 import InputWithMask from "../InputWithMask";
 import SelectInput from "../SelectInput";
-
+import Button from "../Button";
 import "./styles.css";
 
 const validationSchema = Yup.object({
   language: Yup.string().required("language is required"),
+  fax: Yup.string().test(
+    "len",
+    "wrong fax format",
+    (val) => val && val.length === 18
+  ),
+  phones: Yup.array(
+    Yup.string().test(
+      "len",
+      "wrong phone format",
+      (val) => val && val.length === 18
+    )
+  ),
 });
 
 const languages = [
@@ -114,7 +127,18 @@ const languages = [
   },
 ];
 
-const ContactsFormBody = ({ children }) => {
+const ContactsFormBody = ({ children, currentValues: { phones } }) => {
+  const { setFieldValue } = useFormikContext();
+  const addPhoneField = () => {
+    setFieldValue("phones", [...phones, ""]);
+  };
+
+  const deletePhoneField = (i) => () => {
+    const copyPhones = [...phones];
+    copyPhones.splice(i, 1);
+    setFieldValue("phones", copyPhones);
+  };
+
   return (
     <>
       <div style={{ width: "300px" }}>
@@ -144,7 +168,9 @@ const ContactsFormBody = ({ children }) => {
         </InputContainer>
       </div>
       <div>
-        <div style={{ width: "300px", marginBottom: "155px" }}>
+        <div
+          style={{ width: "300px", minHeight: "330px", marginBottom: "115px" }}
+        >
           {/*TODO Fix warning ref*/}
           <InputContainer>
             <InputTitle title="Fax" />
@@ -152,12 +178,46 @@ const ContactsFormBody = ({ children }) => {
               <InputWithMask type="tel" mask="+7 (999) 999 99 99" />
             </FormikField>
           </InputContainer>
-          <InputContainer>
-            <InputTitle title="Phone" />
-            <FormikField name="phone">
-              <Input />
-            </FormikField>
-          </InputContainer>
+          {phones.map((phone, i) => (
+            <InputContainer key={i}>
+              <InputTitle title={`Phone #${i + 1}`} />
+              <FormikField name={`phones[${i}]`}>
+                <InputWithMask type="tel" mask="+7 (999) 999 99 99" />
+              </FormikField>
+              {phones.length > 1 && (
+                <Button
+                  onClick={deletePhoneField(i)}
+                  style={{
+                    border: "none",
+                    minWidth: "9px",
+                    height: "2px",
+                    backgroundColor: "#CED9E5",
+                    padding: 0,
+                    position: "absolute",
+                    top: "50%",
+                    right: "-20px",
+                    cursor: "pointer",
+                  }}
+                  type="button"
+                />
+              )}
+            </InputContainer>
+          ))}
+          {phones.length < 3 && (
+            <button
+              onClick={addPhoneField}
+              type="button"
+              style={{
+                border: "none",
+                background: "none",
+                padding: 0,
+                cursor: "pointer",
+                color: "#657C9A",
+              }}
+            >
+              + add phone number
+            </button>
+          )}
         </div>
         {children}
       </div>
