b966b25c863515b965a3085ba470370984f5fd77
woohoo1607 <woohoo1607@gmail.com>
2021-03-04 09:28:46 +0200

    style: Add horizontal scrolling to tables

---
+++ src/components/UI/TableWrapper/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const TableWrapper = ({ children }) => (
+  <div className="table-wrapper">{children}</div>
+);
+
+export default TableWrapper;
